63d7f75ed2e9ecde3e690fda8fc3aafe005bc458
MrMateo899 <mrmateo899contacto@gmail.com>
2022-12-19 14:00:56 -0500

    MUCHOS CAMBIOS.

--- EJERCICIOS/POTENCIAS.py
+++ EJERCICIOS/POTENCIAS.py
@@ -1,4 +1,4 @@
-var1 = input(int)
-var2 = input(int)
+var1 = int(input("Inserte una base: "))
+var2 = int(input("Inserte un exponente: "))
 
-print(var1**var2)
+print (var1, "elevado a", var2, "es igual a", var1**var2)

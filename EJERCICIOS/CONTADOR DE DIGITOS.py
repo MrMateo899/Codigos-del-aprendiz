17739747fc9b7b74cb0767976e826ba0fc5b4e7d
MrMateo899 <mrmateo899contacto@gmail.com>
2022-12-19 13:42:41 -0500

    TERMINADO EL PROGRAMA DE CONTADOR DE DIGITOS

--- EJERCICIOS/CONTADOR DE DIGITOS.py
+++ EJERCICIOS/CONTADOR DE DIGITOS.py
@@ -1,4 +1,4 @@
 number = input('inserte un numero: ')
 
 if len(number) >1 : print(f'el numero tiene', len(number), "dígitos")
-if len(number) ==1 : print(f'el numero tiene', "un", "dígito")
+if len(number) ==1 : print(f'el numero tiene', "un", "digito")

875235de61f177e455c55206a4e74b2f383c4ebe
MrMateo899 <mrmateo899contacto@gmail.com>
2022-12-19 19:19:02 -0500

    PROGRAMA DE POTENCIAS ARREGLADO

--- EJERCICIOS/POTENCIAS.py
+++ EJERCICIOS/POTENCIAS.py
@@ -3,4 +3,4 @@ var2 = int(input("Inserte un exponente: "))
 
 if var2 == 2 : print(var1, 'al cuadrado es igual a', var1**2 )
 if var2 == 3 : print(var1, 'al cubo es igual a', var1**3 )
-if var2 > 3 : print(var1, 'al cubo es igual a', var1**var2)
+if var2 > 3 : print(var1, 'elevado a', var2, 'es igual a', var1**var2)
